a06ed8f4838d267227700960e566aaf32f4dc256
YukaOtomo <182466613+yuka-om@users.noreply.github.com>
2024-09-25 00:05:25 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,8 +2,25 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		int i;
 		
+
+		
+		for(int c=1 ; c<=100 ; c++) {
+		
+		if( c%3==0 && c%5==0) {
+			System.out.println("FizzBuzz");	
+		}
+		else if(c%3==0) {
+			System.out.println("Fizz");
+		}
+		else if(c%5==0) {
+			System.out.println("Buzz");
+		}
+		else {
+			System.out.println(c);
+		}
+
+		}	
 
 	}
 
